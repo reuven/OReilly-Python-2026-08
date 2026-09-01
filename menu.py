98e8aa8d4b461f16292079a152133a7934fd58cb
Reuven Lerner <reuven@lerner.co.il>
2026-09-01 21:21:34 +0300

    gitautosync update 66

--- menu.py
+++ menu.py
@@ -7,5 +7,8 @@ def menu(options):
     
         print(f'Invalid choice {user_choice}; choose from {options}')
 
-c = menu(['a', 'b', 'c', 'd'])
-print(f'User chose {c}')
+if __name__ == '__main__':
+    # below here will only run when the program is invoked standalone
+    # this stuff will be ignored when we import menu
+    c = menu(['a', 'b', 'c', 'd'])
+    print(f'User chose {c}')
